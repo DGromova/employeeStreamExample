c39e365d185554108c7104c349cbeeb061d00666
Daria Gromova <igromova81@gmail.com>
2023-03-01 19:44:18 +0300

    Initial commit

---
+++ src/main/java/com/example/employeestreamexample/EmployeeStreamExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeestreamexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeStreamExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeStreamExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeestreamexample/EmployeeStreamExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeestreamexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeStreamExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
